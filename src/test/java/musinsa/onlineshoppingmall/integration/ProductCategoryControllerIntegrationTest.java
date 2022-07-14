e312adaa5791a0fe7192fd25fbb1abd0a0719c2e
ikjo93 <auddlr100@naver.com>
2022-07-14 22:25:54 +0900

    test : 신규 상품 카테고리 생성 통합 테스트 코드 작성

---
+++ src/test/java/musinsa/onlineshoppingmall/integration/ProductCategoryControllerIntegrationTest.java
@@ -0,0 +1,50 @@
+package musinsa.onlineshoppingmall.integration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import musinsa.onlineshoppingmall.service.ProductCategoryService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("ProductCategoryController 통합테스트")
+public class ProductCategoryControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductCategoryService productCategoryService;
+
+    @Test
+    @DisplayName("신규 상품 카테고리를 등록할 수 있다.")
+    void 신규_상품_카테고리_등록() throws Exception {
+        // given
+        String requestBody = "{\n"
+            + "    \"name\" : \"신발\"\n"
+            + "}";
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+            post("/api/product-categories")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isOk())
+            .andExpect(jsonPath("$..['id']").value(1))
+            .andExpect(jsonPath("$..['name']").value("신발"));
+    }
+
+}

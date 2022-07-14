16cd0862ef854f9cb51f854d0125ff7a35b3e212
ikjo93 <auddlr100@naver.com>
2022-07-14 21:38:51 +0900

    test : 신규 상품 카테고리 등록 요청 API 컨트롤러 단위 테스트 코드 작성

--- src/test/java/musinsa/onlineshoppingmall/controller/ProductCategoryControllerTest.java
+++ src/test/java/musinsa/onlineshoppingmall/controller/ProductCategoryControllerTest.java
@@ -56,6 +56,95 @@ class ProductCategoryControllerTest {
 
         // then
         resultActions.andExpect(status().isOk())
-            .andExpect(content().json(objectMapper.writeValueAsString(responseBody)));
+            .andExpect(jsonPath("$..['id']").value(1))
+            .andExpect(jsonPath("$..['name']").value("신발"));
+    }
+
+    @Test
+    @DisplayName("신규 상품 카테고리 등록 요청 데이터가 빈값인지 검증한다.")
+    void 신규_상품_카테고리_등록_요청_데이터_빈값_검증() throws Exception {
+        // given
+        String requestBody = "{\n"
+            + "    \"name\" : \"\"\n"
+            + "}";
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+            post("/api/product-categories")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$..['status']").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$..['message']").value("문자 형식의 데이터를 입력해주세요."));
+    }
+
+    @Test
+    @DisplayName("신규 상품 카테고리 등록 요청 데이터가 공백 문자인지 검증한다.")
+    void 신규_상품_카테고리_등록_요청_데이터_공백문자_검증() throws Exception {
+        // given
+        String requestBody = "{\n"
+            + "    \"name\" : \"   \"\n"
+            + "}";
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+            post("/api/product-categories")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$..['status']").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$..['message']").value("문자 형식의 데이터를 입력해주세요."));
+    }
+
+    @Test
+    @DisplayName("신규 상품 카테고리 등록 요청 데이터가 올바른 형식인지 검증한다.")
+    void 신규_상품_카테고리_등록_요청_데이터_형식_검증() throws Exception {
+        // given
+        String requestBody = "{\n"
+            + "    \"name\" : \n"
+            + "}";
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+            post("/api/product-categories")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$..['status']").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$..['message']").value("요청 바디 상 데이터 항목이 잘못된 형식으로 입력되었습니다."));
+    }
+
+    @Test
+    @DisplayName("신규 상품 카테고리 등록 요청 데이터가 널값인지 검증한다.")
+    void 신규_상품_카테고리_등록_요청_데이터_널값_검증() throws Exception {
+        // given
+        String requestBody = "{\n"
+            + "    \"name\" : null\n"
+            + "}";
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+            post("/api/product-categories")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$..['status']").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$..['message']").value("문자 형식의 데이터를 입력해주세요."));
     }
 }

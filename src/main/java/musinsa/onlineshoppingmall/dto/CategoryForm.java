dd8729a727f0921342ed448a72d1853c70736ee4
ikjo93 <auddlr100@naver.com>
2022-07-15 16:18:36 +0900

    fix : 상품 카테고리 폼 클래스 이름 변경

--- src/main/java/musinsa/onlineshoppingmall/dto/CategoryForm.java
+++ src/main/java/musinsa/onlineshoppingmall/dto/CategoryForm.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductCategoryForm {
+public class CategoryForm {
 
     @NotBlank
     private String name;
